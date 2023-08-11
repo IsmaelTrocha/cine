2fe504b5b1a6853ab8970fe4c310caa35fcc9e70
Darkknight-spec <ismael.trocha@hotmail.com>
2023-08-11 18:24:14 -0500

    Actualizar customer

--- src/main/java/com/cineworld/cw/infrastructure/repository/customer/CustomerRepository.java
+++ src/main/java/com/cineworld/cw/infrastructure/repository/customer/CustomerRepository.java
@@ -1,9 +1,7 @@
 package com.cineworld.cw.infrastructure.repository.customer;
 
-import jakarta.transaction.Transactional;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,5 +11,13 @@ public interface CustomerRepository extends JpaRepository<CustomerDto, Long> {
 
   @Query("SELECT c FROM CustomerDto c WHERE c.firstName LIKE %:firstName%")
   List<CustomerDto> findByFirstName(@Param("firstName") String firstName);
+
+  @Query("")
+  CustomerDto updateCustomer(@Param("firstName") String firstName,
+      @Param("secondName") String secondName,
+      @Param("firstSurname") String firstSurname,
+      @Param("secondSurname") String secondSurname,
+      @Param("email") String email
+  );
 
 }
