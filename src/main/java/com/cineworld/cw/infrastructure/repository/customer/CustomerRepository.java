c4c1b0914c911a798d02d3dc83c41392bdcb34b3
Darkknight-spec <ismael.trocha@hotmail.com>
2023-08-12 21:34:04 -0500

    Terminación de Address y Customers.

--- src/main/java/com/cineworld/cw/infrastructure/repository/customer/CustomerRepository.java
+++ src/main/java/com/cineworld/cw/infrastructure/repository/customer/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.cineworld.cw.infrastructure.repository.customer;
 
+import com.cineworld.cw.domain.entities.Customer;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,6 +13,12 @@ public interface CustomerRepository extends JpaRepository<CustomerDto, Long> {
   @Query("SELECT c FROM CustomerDto c WHERE c.firstName LIKE %:firstName%")
   List<CustomerDto> findByFirstName(@Param("firstName") String firstName);
 
+  @Query("UPDATE c SET first_name=:firstName")
+  Customer updateCustomer(
+      @Param("firstName") String firstName,
+      @Param("secondName") String secondName,
+      @Param("firstSurname") String firstSurname,
+      @Param("secondSurname") String secondSurname);
 
 
 }
